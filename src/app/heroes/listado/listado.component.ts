3f636970cbc560fd138c77e7e9d94e951c371313
zimgab <zimram026@gmail.com>
2021-09-02 17:48:29 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman','Ironman','Hulk','Thor','Capitan America'];
+  heroeBorrado: string = '';
+  borrarHeroe(): any{
+   
+     this.heroeBorrado = this.heroes.shift() || '';
+      
+     
+
+     
+    
+  }
+
+ 
+
+}
